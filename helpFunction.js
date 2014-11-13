add36cd9ad80f38d6c7107eadd3661995e1f54e2
SaravananRajaraman <rsaravanan.btech@gmail.com>
2014-11-13 17:04:40 +0530

    Implementation of helper functions collections

---
+++ helpFunction.js
@@ -0,0 +1,12 @@
+//--------Check the duplicate value to push------------
+function hasDuplicates(arr,value) {    
+        if (arr.indexOf(value) !== -1) {
+            return true;
+        }      
+    return false;
+}
+ 
+//example:
+var arr=[0,1,2,3,4];
+var value=10;
+console.log(hasDuplicates(arr,value));
